0cf7b2f07391bb2a2a5161774138767a0e8dc6d2
patworld <86130447+patworld@users.noreply.github.com>
2022-04-12 21:27:44 +0530

    Add files via upload

---
+++ ScrapBin/php/config/db.php
@@ -0,0 +1,10 @@
+<?php
+  // Create Connection
+   $conn = mysqli_connect('localhost','root','Priphp19','scrapbin');
+  //  Check Connection
+  if(mysqli_connect_errno()){
+    //Connection Failed
+    echo "Connection Failed: ".mysqli_connect_errno();
+  }
+  //4Q*_u5uUrVt(wY#
+?>
